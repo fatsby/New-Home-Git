4f8df08b228093aa95f085920fdec7a45cc91dcc
Le Minh Tri <jackcheatpigg@gmail.com>
2025-06-08 20:48:26 +0700

    added go to top button

--- utilities.js
+++ utilities.js
@@ -6,3 +6,15 @@ toggler.addEventListener('click', function () {
         toggler.setAttribute('name', 'menu');
     }
 });
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+window.addEventListener('scroll', function () {
+    const button = document.querySelector('.go-to-top');
+    if (window.scrollY > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+});
